0432d5ca1f2b27b9d09d8aded0eb5de4f1e56146
arcanephoenix <chinmaypai0451@gmail.com>
2019-03-18 16:34:04 -0400

    v0.9 First complete build
    First build working. Playable version to be added soon.

---
+++ Assets/SyncAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncAudio : MonoBehaviour
+{
+    public AudioSource source1;
+    public AudioSource source2;
+    public AudioSource source3;
+    public AudioSource source4;
+    public AudioSource source5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        source2.timeSamples = source1.timeSamples;
+        source3.timeSamples = source1.timeSamples;
+        source4.timeSamples = source1.timeSamples;
+        source5.timeSamples = source1.timeSamples;
+    }
+}
