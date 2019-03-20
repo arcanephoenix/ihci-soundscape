6ad104825983c3526c89336f734b80d0deb915ec
arcanephoenix <chinmaypai0451@gmail.com>
2019-03-20 00:02:45 -0400

    v1.0
    Included executable and slight changes. This is probably the final build, beyond any bug fixes.

---
+++ Assets/TrainHorn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainHorn : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(SoundsTick());
+    }
+
+    IEnumerator SoundsTick()
+    {
+        while (true)
+        {
+            audioSource.Play();
+            yield return new WaitForSeconds(20f);
+        }
+    }
+}
